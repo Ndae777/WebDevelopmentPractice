ac906db50b85e5870dc70f2515fc94a6512f5e4d
Ndae777 <nkhumelenindae777@gmail.com>
2026-02-21 00:11:39 +0200

    Add models to bids model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,7 +9,11 @@ class Auction_listings(models.Model):
     pass
 
 class Bids(models.Model):
-    pass
+    bidder_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    bid_amount = models.IntegerField()
+
+    def __str__(self):
+        return f"Bid {self.id} from {self.bidder_id.username} is ${self.bid_amount}"
 
 class Comments(models.Model):
-    pass
+    pass    
