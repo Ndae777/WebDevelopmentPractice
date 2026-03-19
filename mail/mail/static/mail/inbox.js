9097fa163b4121ff60383e0c30fca62d0e63fa9d
Ndae777 <nkhumelenindae777@gmail.com>
2026-03-19 16:45:38 +0200

    Mailbox function done , update for read fun

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('Inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#inbox').addEventListener('click', () => { load_mailbox('Inbox'); mailbox('inbox') });
+  document.querySelector('#sent').addEventListener('click', () => { load_mailbox('sent'); mailbox('sent') });
+  document.querySelector('#archived').addEventListener('click', () => { load_mailbox('archive'); mailbox('archive') });
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector("#compose-form").onsubmit = send_mail;
 
@@ -24,7 +24,7 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -34,29 +34,50 @@ function load_mailbox(mailbox) {
 
 }
 
-function send_mail(){
-
+function send_mail() {
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-        recipients: document.querySelector("#compose-recipients").value,
-        subject: document.querySelector("#compose-subject").value,
-        body: document.querySelector("#compose-body").value,
-      })
-    }).then(response => response.json())
+      recipients: document.querySelector("#compose-recipients").value,
+      subject: document.querySelector("#compose-subject").value,
+      body: document.querySelector("#compose-body").value,
+    })
+  }).then(response => response.json())
     .then(email_composed => {
       console.log(email_composed);
       load_mailbox('sent');
     })
-    return false;
+  return false;
 }
 
-function mailbox(){
 
-  fetch('/emails/<mailbox>')
-  .then(response => response.json)
-  .then(mail => {
-    console.log(mail)
-  })
+function mailbox(mailbox_name) {
 
+
+  fetch(`/emails/${mailbox_name}`)
+    .then(response => response.json())
+    .then(mail => {
+
+      //console.log("Latest email in this mailbox: \n", mail[0]) //checking the data
+
+      let mail_count = 0;
+
+      mail.forEach(
+        () => {
+          
+          //creating and styling div
+          let div = document.createElement('div');
+          div.innerHTML = `<div><b>${mail[mail_count]['sender']}</b> ${mail[mail_count]['subject']}</div> <div style='color:grey;'>${mail[mail_count]['timestamp']}</div>`
+          div.style.border = "1px solid black";
+          div.style.padding = "10px";
+          div.style.display = "flex";
+          div.style.justifyContent = "space-between";
+          document.querySelector("#emails-view").append(div)
+
+          mail_count++;
+        }
+      )
+
+
+    })
 }
