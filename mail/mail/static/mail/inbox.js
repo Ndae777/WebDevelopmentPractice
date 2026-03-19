bd4018a27fdde405ac2d8d5643852d15245f63f5
Ndae777 <nkhumelenindae777@gmail.com>
2026-03-19 14:53:45 +0200

    Create Send mail function

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('Inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector("#compose-form").onsubmit = send_mail;
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,32 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+}
+
+function send_mail(){
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector("#compose-recipients").value,
+        subject: document.querySelector("#compose-subject").value,
+        body: document.querySelector("#compose-body").value,
+      })
+    }).then(response => response.json())
+    .then(email_composed => {
+      console.log(email_composed);
+      load_mailbox('sent');
+    })
+    return false;
+}
+
+function mailbox(){
+
+  fetch('/emails/<mailbox>')
+  .then(response => response.json)
+  .then(mail => {
+    console.log(mail)
+  })
+
 }
